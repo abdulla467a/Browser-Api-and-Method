49e6d4b2c4ea213c09305c9884b296196fe67a5c
abdulla467a <ayanrahman009@gmail.com>
2021-10-25 20:08:19 +0600

    first commit

---
+++ explore.js
@@ -0,0 +1,13 @@
+const name = 15;
+const student = {
+    name: 'Jolil', friend: 'borsha', job: 'Business',
+    makeMovie: function (name) {
+        console.log(name);
+    }
+};
+student.makeMovie('Din raat');
+// document.getElementById('name');
+alert('abc')
+
+
+//kaj korbe na because DOM always html er shate kaj kore.
